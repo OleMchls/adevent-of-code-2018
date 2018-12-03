4020f345d96421787962b5a402a9d2951089f0f4
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-12-03 11:56:40 +0100

    initial commit

---
+++ lib/aoc18.ex
@@ -0,0 +1,18 @@
+defmodule Aoc18 do
+  @moduledoc """
+  Documentation for Aoc18.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc18.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc18_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc18Test do
+  use ExUnit.Case
+  doctest Aoc18
+
+  test "greets the world" do
+    assert Aoc18.hello() == :world
+  end
+end
